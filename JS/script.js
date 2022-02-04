2946023a15102398ce617f08f870eb2157ca60ca
Pavitra Gannavarapu <pagannav@in.ibm.com>
2022-02-04 18:13:09 +0530

    Newly Added

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,6 @@ request.open('GET',"https://raw.githubusercontent.com/rvsp/restcountries-json-da
 request.send();
 request.onload = function(){
     var data = JSON.parse(request.response);
-    var j=data.length;
-    for (var i=0;i<j;i++)
-    {
-        console.log(data[i].name,data[i].population);
+    console.log(data);
     }
-}
+
